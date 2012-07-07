bbab7145ad09c1764c6f2b88b5a59f4b2aaec4e8
Ernesto Bossi <bossi.ernestog@gmail.com>
2012-07-07 19:03:40 -0300

    Added RegEx checking to proxyTransport

--- libgravatar/xmlrpc/proxyTransport.py
+++ libgravatar/xmlrpc/proxyTransport.py
@@ -1,4 +1,6 @@
 import xmlrpclib, httplib
+import re
+from ..exceptions import ProxyFormatException
 
 class ProxiedTransport(xmlrpclib.Transport):
 
@@ -7,6 +9,7 @@ class ProxiedTransport(xmlrpclib.Transport):
         super(ProxiedTransport)
 
     def set_proxy(self, proxy):
+        self.checkProxy(proxy)
         self.proxy = proxy
     def make_connection(self, host):
         self.realhost = host
@@ -16,3 +19,8 @@ class ProxiedTransport(xmlrpclib.Transport):
         connection.putrequest("POST", 'http://{1}{2}'.format(self.realhost, handler))
     def send_host(self, connection, host):
         connection.putheader('Host', self.realhost)
+    def checkProxy(self,proxy):
+        regex=re.compile(r'\S+:[-+]?(0[xX][\dA-Fa-f]+|0[0-7]*|\d+)@\S+:\S+') #Simple RegEx to match the proxy definition
+        if regex.match(proxy) is None:
+            raise ProxyFormatException('Invalid proxy configuration format, should be like host:port@user:password')
+
