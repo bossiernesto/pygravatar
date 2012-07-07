b9d01f4559aa5a1476b13248b88335d20b24caf1
Ernesto Bossi <bossi.ernestog@gmail.com>
2012-07-07 18:29:32 -0300

    Added RegEx Exception

--- libgravatar/exceptions.py
+++ libgravatar/exceptions.py
@@ -2,3 +2,11 @@ class PyGravatarConnectException(Exception):
 
     def __init__(self, *args, **kwargs):
         Exception.__init__(self, *args, **kwargs)
+
+
+class ProxyFormatException(Exception):
+
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
