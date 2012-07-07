1e9901c9aae0a0f25386e63755fa855bf5157c21
Ernesto Bossi <bossi.ernestog@gmail.com>
2012-07-07 19:12:36 -0300

    Extracted proxy checking out of the Class and moved to xmlrpcConnector

--- libgravatar/xmlrpc/proxyTransport.py
+++ libgravatar/xmlrpc/proxyTransport.py
@@ -1,6 +1,5 @@
 import xmlrpclib, httplib
-import re
-from ..exceptions import ProxyFormatException
+
 
 class ProxiedTransport(xmlrpclib.Transport):
 
@@ -19,8 +18,4 @@ class ProxiedTransport(xmlrpclib.Transport):
         connection.putrequest("POST", 'http://{1}{2}'.format(self.realhost, handler))
     def send_host(self, connection, host):
         connection.putheader('Host', self.realhost)
-    def checkProxy(self,proxy):
-        regex=re.compile(r'\S+:[-+]?(0[xX][\dA-Fa-f]+|0[0-7]*|\d+)@\S+:\S+') #Simple RegEx to match the proxy definition
-        if regex.match(proxy) is None:
-            raise ProxyFormatException('Invalid proxy configuration format, should be like host:port@user:password')
 
