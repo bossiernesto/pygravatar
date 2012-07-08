28d2926ddf15236316e21284a95456d4a73c8d65
Ernesto Bossi <bossi.ernestog@gmail.com>
2012-07-07 21:53:09 -0300

    Cleaned code

--- libgravatar/xmlrpc/xmlrpcConnector.py
+++ libgravatar/xmlrpc/xmlrpcConnector.py
@@ -1,10 +1,10 @@
-import xmlrpclib
+import xmlrpclib,re,proxyTransport
+from ..exceptions import ProxyFormatException
 
+class xmlRpcConnector():
+    """rpcUrl [,options] -> a logical connection to an XML-RPC server
 
-class xmlRpcConnector(xmlrpclib.ServerProxy):
-    """uri [,options] -> a logical connection to an XML-RPC server
-
-        uri is the connection point on the server, given as
+        rpcUrl is the connection point on the server, given as
         scheme://host/target.
 
         If the target part and the slash preceding it are both omitted,
@@ -12,16 +12,30 @@ class xmlRpcConnector(xmlrpclib.ServerProxy):
 
         The following options can be given as keyword arguments:
 
-            transport: a transport factory
-            encoding: the request encoding (default is UTF-8)
-
-        All 8-bit strings passed to the server proxy are assumed to use
-        the given encoding.
+            proxy: configure connector if behind a Proxy Server, parameter should be like "host:port@user:password"
         """
+
+    def init(self,rpcUrl,proxy=None):
+        if proxy is not None:
+            self.checkProxy(proxy)
+            self.transport=proxyTransport.ProxiedTransport(proxy)
+        else:
+            self.transport=None
+        self.rpc=xmlrpclib.ServerProxy(rpcUrl,transport=self.transport)
+
+    def checkProxy(self,proxy):
+        """Check proxy format "host:port@user:password" """
+        regex=re.compile(r'\S+:[-+]?(0[xX][\dA-Fa-f]+|0[0-7]*|\d+)@\S+:\S+') #Simple RegEx to match the proxy definition
+        if regex.match(proxy) is None:
+            raise ProxyFormatException('Invalid proxy configuration format, should be like host:port@user:password')
+
     def call(self,name,*args,**kwargs):
         """This function is used to call any method provided by the Gravatar api"""
-        #TODO: do a type checking of **kwargs
-        pass
+        try:
+            arguments=kwargs
+            return getattr(self.rpc,name)(*arguments)
+        except Exception,e:
+            pass
 
 
 
